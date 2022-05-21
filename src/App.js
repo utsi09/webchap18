06ae2b36ed14bc89dd329214f22b761bedda5e58
utsi09 <92018822+utsi09@users.noreply.github.com>
2022-05-21 22:32:49 +0900

    🌱 add GundamTitle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./styles.css";
 
-const Playlist = () => {
-  return <h3>꼭한번만나고싶다</h3>;
+const GundamTitle = () => {
+  return <h1>건담 스토리</h1>;
+};
+
+const GundamHeader = () => {
+  return <h3>건담</h3>;
 };
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Playlist />
+      <GundamTitle />
+      <GundamHeader />
+      <GundamHeader />
     </div>
   );
 }
