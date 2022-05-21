fbfc34e01b59b3b2eb1312239ea3b02c9e5fcbf6
utsi09 <92018822+utsi09@users.noreply.github.com>
2022-05-21 22:20:16 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+const Playlist = () => {
+  return <h3>꼭한번만나고싶다</h3>;
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Playlist />
+      <Playlist />
+      <Playlist />
+      <Playlist />
+    </div>
+  );
+}
