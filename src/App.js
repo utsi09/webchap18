2a26b0f33f204d6d38f4cb131f2399197c0c8f58
utsi09 <92018822+utsi09@users.noreply.github.com>
2022-05-21 22:23:31 +0900

    🔨 edit App component

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <Playlist />
-      <Playlist />
-      <Playlist />
-      <Playlist />
     </div>
   );
 }
